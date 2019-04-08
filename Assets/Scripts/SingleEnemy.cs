2c0d75ba68f517623935f8174bba0daeae3b4b66
Floris <Flomo123456789@gmail.com>
2019-04-08 10:59:09 +0100

    done

---
+++ Assets/Scripts/SingleEnemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleEnemy : Tower
+{
+    protected override void Shoot()
+    {
+        print("shoot one guy");
+    }
+
+    protected override bool HasTarget()
+    {
+        // logica voor targetting singleEnemy
+        var cols = Physics.OverlapSphere(transform.position, _radius, _layer);
+        if (cols.Length > 0)
+            _target = cols[0].transform;
+
+        print("target is: " + _target);
+        return true;
+    }
+}
